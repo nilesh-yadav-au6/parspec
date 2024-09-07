0260c62d13d723f818f95bf22b24d57cf1c62ea0
Nilesh Yadav <nileshyadav@Nileshs-MacBook-Pro.local>
2024-09-07 23:59:45 +0530

    newchange2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import SearchPage from "./pages/SearchPage";
 
 const App = () => {
   return (
-    <div className="flex justify-center mt-10">
+    <div className="flex justify-center mt-8">
       <SearchPage />
     </div>
   );
